c48146f552b282331f4d51daaa7f36d6e9f90694
Anik98B <anikbhowmik0188@gmail.com>
2024-02-14 19:03:06 +0600

    setup to playground with js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -9,7 +9,21 @@
 //     // console.log(playgroundSection.classList)
 // }
 
+function continueGame(){
+    // generate a random alphabet
+    const alphabet= getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    //get randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement= document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText= alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
